955c7d276e8de890a06825cf665fd690f0a68ab0
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2023-01-29 00:56:38 +0200

    astro config

--- src/animate.ts
+++
@@ -1,9 +0,0 @@
-export default function animateInOnScroll(element: Element) {
-    const {top, bottom} = element.getBoundingClientRect();
-    if (top > 0.75 * window.innerHeight || bottom < .25 * window.innerHeight) {
-        element.classList.add('hide')
-    }
-    else {
-        element.classList.remove('hide')
-    }
-}
